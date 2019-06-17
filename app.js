0b886b4170ebda49561e090e09c85245322c8771
Sharma, Rakesh <rakesh.sharma@dish.com>
2019-06-17 10:19:14 -0600

    Sample

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var app = express();
-
+const port = 4000;
 app.get('/', function(req, resp){
 	var i;
-	for(i = 0; i < 100; i++) {
-		console.log('Example app listening on port 3000!');	
+	const loop_times = 10;
+	for(i = 0; i < loop_times; i++) {
+		console.log('App running');	
 	}
 	resp.send("Hello World!!");
 });
 
-app.listen(3000, function (){
-	console.log('Example app listening on port 3000!');
+app.listen(port, function (){
+	console.log(`Example app listening on port ${port}`);
 });
